35add2e3bf137c0f5c1c8e46cd47025eb5e8adff
Jesus Peralta <chagitodeveloper@gmail.com>
2026-03-06 17:56:20 -0800

    Added systemMatrix.py
    - Updated the README.md
    - Added the systemMatrix code
    - Combined imaging.py with another code to make visualization.py

--- Jesus' Work/visualization.py
+++ Jesus' Work/visualization.py
@@ -1,5 +1,7 @@
 import numpy as np
 from PIL import Image
+from geometry import Ray
+from matplotlib.axes import Axes
 
 def MakeSinogram(inputarray, filename, numberofrotations, detectors):
     max = np.max(inputarray)
@@ -10,3 +12,8 @@ def MakeSinogram(inputarray, filename, numberofrotations, detectors):
     output = np.abs(np.reshape((np.floor(inputarray * scale)).astype(np.int8), (numberofrotations, detectors)))
     outputimage = Image.fromarray(output, "L")
     outputimage.save(filename)
+
+def PlotAMatrix(AMatrix, ax: Axes): 
+    rays: list[Ray] = AMatrix.Rays
+    for i in rays:
+        ax.plot([i.point1.x, i.point2.x], [i.point1.y, i.point2.y])
